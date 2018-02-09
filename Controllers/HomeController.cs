b7e13620a5cae08c97cb0af8a84b21f482c2b5d6
Ian Goodrich <iangoodrich@hotmail.com>
2018-02-09 11:13:07 -0800

    buildout app's basic framework returning no build errors.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using GENERICTEMPLATEXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX.Models
+using System;
+
+namespace GENERICTEMPLATEXXXXXXXXXXXXXXXXXXXXXXXXXXXX.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+
+    }
+}
---
+++ Models/GENERICTEMPLATE.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System;
+
+
+namespace GENERICTEMPLATEXXXXXXXXXXXXXXXXXXXXX.Models
+{
+    public class GENERTIC_TEMPLATEXXXXXXXX
+    {
+
+
+    }
+
+{
